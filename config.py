a42282ca51a2c71e2573bc4dd8ace8be4c9cc96a
Praveena0308 <praveenabolli03@gmail.com>
2024-08-30 14:55:41 +0200

    Update config.py

--- config.py
+++ config.py
@@ -1,5 +1,5 @@
 import os
 
 class Config:
-    SQLALCHEMY_DATABASE_URI = os.getenv('DATABASE_URL', 'postgresql://postgres:password@localhost:5432/flask_db')
+    SQLALCHEMY_DATABASE_URI = os.getenv('DATABASE_URL', 'postgresql://flask_user:1234@localhost:5432/flask_db?sslmode=disable')
     SQLALCHEMY_TRACK_MODIFICATIONS = False
