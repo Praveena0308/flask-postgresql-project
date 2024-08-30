4e32e9e2711b431195218cde36ab65b8064fcb04
PraveenaBolli123 <bollipraveena99@gmail.com>
2024-08-30 10:08:27 +0200

    new

---
+++ app.py
@@ -0,0 +1,47 @@
+from flask import Flask, jsonify, request
+from flask_sqlalchemy import SQLAlchemy
+from models import db, User
+
+app = Flask(__name__)
+app.config.from_object('config.Config')
+
+db.init_app(app)
+
+@app.route('/users', methods=['POST'])
+def add_user():
+    data = request.json
+    new_user = User(name=data['name'], email=data['email'])
+    db.session.add(new_user)
+    db.session.commit()
+    return jsonify({"message": "User added", "user": new_user.serialize()}), 201
+
+@app.route('/users', methods=['GET'])
+def get_users():
+    users = User.query.all()
+    return jsonify([user.serialize() for user in users]), 200
+
+@app.route('/users/<int:user_id>', methods=['GET'])
+def get_user(user_id):
+    user = User.query.get_or_404(user_id)
+    return jsonify(user.serialize()), 200
+
+@app.route('/users/<int:user_id>', methods=['PUT'])
+def update_user(user_id):
+    user = User.query.get_or_404(user_id)
+    data = request.json
+    user.name = data['name']
+    user.email = data['email']
+    db.session.commit()
+    return jsonify({"message": "User updated", "user": user.serialize()}), 200
+
+@app.route('/users/<int:user_id>', methods=['DELETE'])
+def delete_user(user_id):
+    user = User.query.get_or_404(user_id)
+    db.session.delete(user)
+    db.session.commit()
+    return jsonify({"message": "User deleted"}), 200
+
+if __name__ == '__main__':
+    with app.app_context():
+        db.create_all()
+    app.run(host='0.0.0.0', port=5000)
